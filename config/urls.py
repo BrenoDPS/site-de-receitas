3c653a53bf46e257c21cbaf615c0bbe441c051e8
BrenoDPS <brenodepadua7@gmail.com>
2024-06-18 04:44:50 -0300

    added readme file and other changes

--- config/urls.py
+++ config/urls.py
@@ -21,10 +21,10 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('recipes.urls')),
-    path('register/', user_views.register, name="user-register"),
+    path('api/v1/', include('recipes.urls')),
+    path('api/v1/register/', user_views.register, name="user-register"),
     # new urls
-    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name="user-login"),
-    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name="user-logout"),
-    path('profile/', user_views.profile, name="user-profile"),
+    path('api/v1/login/', auth_views.LoginView.as_view(template_name='users/login.html'), name="user-login"),
+    path('api/v1/logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name="user-logout"),
+    path('api/v1/profile/', user_views.profile, name="user-profile"),
 ]
--- recipes/models.py
+++ recipes/models.py
@@ -2,12 +2,24 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
 
-# Create your models here.
+class Category(models.Model):
+  name = models.CharField(max_length=100)
+  image = models.URLField(max_length=255, blank=True, null=True)
+
+  def __str__(self):
+    return self.name
+
+
 class Recipe(models.Model):
-  title = models.CharField(max_length=100)
+  category = models.ForeignKey(Category, related_name='recipe', on_delete=models.CASCADE, default=None)
+  name = models.CharField(max_length=100)
   description = models.TextField()
 
-  ingredientes = models.CharField(max_length=255, default="Valor padrão")
+  ingredients = models.JSONField()
+  preparation = models.JSONField()
+  time = models.CharField(max_length=50)
+  image = models.URLField(max_length=255, blank=True, null=True)
+  serve = models.CharField(max_length=50)
 
   author = models.ForeignKey(User, on_delete=models.CASCADE)
 
@@ -18,4 +30,4 @@ class Recipe(models.Model):
       return reverse("recipes-detail", kwargs={"pk": self.pk})
 
   def __str__(self):
-    return self.title
+    return self.name
